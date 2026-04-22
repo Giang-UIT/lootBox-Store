508296320ceaa7791dd78385cb9543301e282034
Giang Hoang Nguyen <Gng000@uit.no>
2026-04-22 19:23:10 +0200

    changed api.ts to match the url of the back end

--- Frontend/src/api.ts
+++ Frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://django-back-end-pfnc.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
